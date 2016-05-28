c73497c51b07c1886543e4afb81c916e2b0a0198
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-05-28 16:53:16 +0700

    .

--- source/apps/frontend/protected/components/widgets/views/right.php
+++ source/apps/frontend/protected/components/widgets/views/right.php
@@ -1,5 +1,5 @@
 <?php 
-$url = 'https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=91+Th%C3%A0nh+Th%C3%A1i,+14,+Qu%E1%BA%ADn+10,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=37.188995,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=91+Th%C3%A0nh+Th%C3%A1i,+14,+Qu%E1%BA%ADn+10,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam&amp;ll=10.7736571,106.6646504&amp;spn=0.005638,0.010568&amp;t=m&amp;z=14&amp;output=embed';
+$url = 'https://goo.gl/maps/TMN92ELrJr62';
 ?>
 <div id="center-right"> 
         <!-- .support-address -->
